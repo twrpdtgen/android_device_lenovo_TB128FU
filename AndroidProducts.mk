a676b45eeac8f7dd547bfed2819c14e5c9464d21
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-25 09:59:49 +0200

    TB128FU: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: lenovo Model: Lenovo TB128FU Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TB128FU.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TB128FU-user \
+    omni_TB128FU-userdebug \
+    omni_TB128FU-eng
